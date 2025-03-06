0de5567fdc1b098d92defcf0e81f71f35ed03d99
Rita Wanjiku <mwangirita@gmail.com>
2025-03-06 18:12:57 +0300

    Updated query_samples.py to use .get() as per Betty requirements

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,36 +2,27 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 author_name = "Author Name"
-author = Author.objects.filter(name=author_name).first()
-
-if author:
+try:
+    author = Author.objects.get(name=author_name)
     books_by_author = Book.objects.filter(author=author)
     print("Books by", author_name, ":", [book.title for book in books_by_author])
-else:
+except Author.DoesNotExist:
     print(f"No author found with the name {author_name}")
-
+    
 # List all books in a library
 library_name = "Library Name"
-library = Library.objects.filter(name=library_name).first()
-
-if library:
+try:
+    library = Library.objects.get(name=library_name)
     books_in_library = library.books.all()
     print("Books in", library_name, ":", [book.title for book in books_in_library])
-else:
+except Library.DoesNotExist:
     print(f"No library found with the name {library_name}")
 
 # Retrieve the librarian for a library
 library_name = "Library Name"
-library = Library.objects.filter(name=library_name).first()
-
-if library:
-    librarian = Librarian.objects.filter(library=library).first()
-    if librarian:
-        print(f"The librarian for {library_name} is {librarian.name}")
-    else:
-        print(f"No librarian found for {library_name}")
-
-else:
-    print(f"No library found with the name {library_name}")
-
-
+try:
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+    print(f"The librarian for {library_name} is {librarian.name}")
+except (Library.DoesNotExist, Librarian.DoesNotExist):
+    print(f"No librarian found for {library_name}")
