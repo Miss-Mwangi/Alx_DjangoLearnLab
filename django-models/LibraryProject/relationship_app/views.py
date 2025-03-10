655d45014bf674615e639e4fab9eeead0ab68410
Rita Wanjiku <mwangirita@gmail.com>
2025-03-10 14:02:53 +0300

    Fixed class-based view for library details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,5 +11,5 @@ def list_books(request):
 # Class-Based View to show details of a specific Library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html" 
+    template_name = "relationship_app/library_detail.html" 
     context_object_name = "library"
