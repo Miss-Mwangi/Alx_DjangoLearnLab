7112cc6f5c25bf26cf2dd2164be697ea8e2c302d
Rita Wanjiku <mwangirita@gmail.com>
2025-03-21 06:24:01 +0300

    Updated UserProfile model, added role-based access control, and migrations

--- django-models/LibraryProject/relationship_app/signals.py
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -6,5 +6,5 @@ from .models import UserProfile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance, role='Member')
 
