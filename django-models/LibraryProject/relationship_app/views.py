2ba20a9e1bfafb2448c981e8f129842ebd6f56f5
Rita Wanjiku <mwangirita@gmail.com>
2025-03-21 15:05:48 +0300

    Updated views with permission_required decorator

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import permission_required, user_passes_test
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
-from django.contrib.auth.decorators import user_passes_test, permission_required
 from django.http import HttpResponse
 from .forms import BookForm
 
