663e34ccf6793ad5a4efa017a673d8fd35df4255
Rita Wanjiku <mwangirita@gmail.com>
2025-03-21 15:27:41 +0300

    Updated URLs and fixed Django errors

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_view, name='admin_view'),
     path('librarian-panel/', views.librarian_view, name='librarian_view'),
     path('member-section/', views.member_view, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.delete_book, name='delete_book')
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
