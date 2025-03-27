ccc1622f4e72f360dfb19a47e7b8cc43389a978f
Rita Wanjiku <mwangirita@gmail.com>
2025-03-27 18:23:35 +0300

    Debugging errors

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import Book
 
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
 class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
