2ca563eab36d08c8a5fb8ac7b9d59dae47150eeb
Rita Wanjiku <mwangirita@gmail.com>
2025-03-29 15:11:59 +0300

    Changes and debugging

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -48,6 +48,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 MIDDLEWARE = [
