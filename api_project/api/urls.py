7aab3abaf9410ede706076c197ec24efe26612d9
Rita Wanjiku <mwangirita@gmail.com>
2025-03-29 14:39:45 +0300

    Push latest changes before debugging

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import BookList, BookViewSet
 
 router = DefaultRouter()
@@ -8,4 +9,6 @@ router.register(r'books_all', BookViewSet, basename='book')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
